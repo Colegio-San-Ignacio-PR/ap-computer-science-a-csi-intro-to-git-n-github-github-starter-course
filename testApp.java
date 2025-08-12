32b7111669ec7735aae6b2126525ffbd679c1731
Reynaldo Belfort Pierrilus <167442029+rbelfortcsi@users.noreply.github.com>
2025-08-12 06:19:23 -0400

    Add files via upload

---
+++ testApp.java
@@ -0,0 +1,18 @@
+//Code by Reynaldo Belfort, SJ
+
+public class testApp {
+    
+    public static void main(String[] args){
+        System.out.println("This is a test...!");
+
+        System.out.println("This is another line here..");
+
+        printCustomMessage();
+
+    }
+
+    static void printCustomMessage(){
+
+        System.out.println("====== weeepaaa =======");
+    }
+}
